b60c84547ba20dfd5442563f5fadb05a4969dc3f
kyrillos-gaber <kyrillosgaber@gmail.com>
2023-02-22 17:53:52 +0200

    fix line

--- database/migrations/2023_02_22_134043_create_invoices_table.php
+++ database/migrations/2023_02_22_134043_create_invoices_table.php
@@ -39,8 +39,8 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::dropIfExists('invoices');
-    }
+    // public function down()
+    // {
+    //     Schema::dropIfExists('invoices');
+    // }
 };
