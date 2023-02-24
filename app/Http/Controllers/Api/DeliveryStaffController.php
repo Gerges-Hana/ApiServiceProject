2c4dfebfdc7ac654f959cf5071c0e0e9a0ca0478
kyrillos-gaber <kyrillosgaber@gmail.com>
2023-02-24 17:58:31 +0200

    create DeliveryStaff model

--- app/Http/Controllers/Api/DeliveryStaffController.php
+++ app/Http/Controllers/Api/DeliveryStaffController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Delivery_Staff;
 use Illuminate\Http\Request;
 
 class DeliveryStaffController extends Controller
 {
-    //
+    public function index()
+    {
+        dd(Delivery_Staff::all());
+        return Delivery_Staff::all();
+    }
 }
---
+++ app/Models/Delivery_Staff.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Delivery_Staff extends Model
+{
+    use HasFactory;
+
+    // private $fillable = [
+    //     'id',
+    //     'companyId',
+    //     'name',
+    //     'userName',
+    //     'nationalId',
+    //     'phone',
+    //     'salary',
+    // ];
+}
